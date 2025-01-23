b21d3b5564f1d398ce8cfca3f78ebb3dd0eec390
stepan <stepanlezennikov@gmail.com>
2025-01-23 12:13:55 +0300

    Миграции работают теперь

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.database import Base
+from src.app.database import Base
+from src.app.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- src/app/database.py
+++ src/app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from src.app.models.user import User
 
 SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/user_management"
 
