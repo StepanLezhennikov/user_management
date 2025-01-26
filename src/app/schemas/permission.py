27ed2a7feb0018021b5f2ed32a0b31b93788714e
stepan <stepanlezennikov@gmail.com>
2025-01-26 14:19:44 +0300

    Добавил и изменил Pydantic схемы

---
+++ src/app/schemas/permission.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class PermissionBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    name: str
+    description: str
+
+
+class Permission(PermissionBase):
+    id: int
